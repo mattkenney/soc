8617553ab67d1cacfc7c5a4756be82fcf26d71ca
Matt Kenney <Matt Kenney>
2016-02-14 13:49:05 -0500

    add copyright notice

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
+#
+# Copyright 2016 Matt Kenney
+#
+# This file is part of Soc.
+#
+# Soc is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Fotocog is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Soc.  If not, see <http://www.gnu.org/licenses/>.
+#
 require 'haml'
 require 'omniauth-twitter'
 require 'rack/session/redis'
